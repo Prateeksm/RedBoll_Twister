66f8107ac8a32e92b780123a6ba132f93cff6b60
Prateeksm <prateek.s877@gmail.com>
2019-07-12 15:31:15 +0530

    RedBoll Twister Game

---
+++ Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float gameDelay = 2f;
+
+    public GameObject completeLevelUI;
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("gameend");
+            Invoke("Restart",gameDelay);
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform Player;
+    public Vector3 offSet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Player.position + offSet;
+    }
+}
